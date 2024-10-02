c3e6616702e7788fa4acc144a8f77fb605e68e3a
DominicS97 <dejfsutcliffe@gmail.com>
2024-10-02 08:03:03 +0100

    moved to public project

---
+++ CarClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CarClass
+{
+    // Create car class
+    public class Car
+    {
+        // fields
+        public string make;
+        public string model;
+        public string reg;
+        public int year;
+        public int value;
+
+        // methods
+        public void returnValue()
+        {
+            Console.WriteLine(this.value);
+        }
+
+        public void returnYear()
+        {
+            Console.WriteLine(this.year);
+        }
+
+        // class constructor
+        public Car(string make, string model, string reg, int year, int value)
+        {
+            this.make = make;
+            this.model = model;
+            this.reg = reg;
+            this.year = year;
+            this.value = value;
+        }
+
+        // override tostring
+        public override string ToString()
+        {
+            return "Car details: (Make: " + make + " Model: " + model + " Registration: " + reg + " Year of registration: " + year + " Current value: £" + value + ")";
+        }  
+    }
+    class Test
+    {
+        static void Main(string[] args) 
+        {
+            Car newCar = new Car("Kia", "Sorento", "ABCDEF", 2015, 15000);
+
+            Console.WriteLine(newCar.ToString());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# C--Class-Assessment
+# C#-Class-Assessment
+
+-   Car class with fields for the car’s make, model, registration, year of registration, and current value.
+
+-   Constructor for the class which constructs an object with values for each field.
+
+-   Methods which return the current value of the car and the year of registration of the car.
+
+-   Override ToString to return a string containing all information about the car.
+
+-   Test class which constructs a Car and prints its details to the console.
